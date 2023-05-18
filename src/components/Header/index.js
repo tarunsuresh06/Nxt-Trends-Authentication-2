0a81a7afef5bc0b2f6a4929212e115b3c7ea76ad
tommy <tarunsuresh.v@gmail.com>
2023-05-18 12:04:15 +0000

    Add Authentication for Login & Logout / Add Link on Header Component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,69 +1,94 @@
+import {Link, withRouter} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import './index.css'
 
-const Header = () => (
-  <nav className="nav-header">
-    <div className="nav-content">
-      <div className="nav-bar-mobile-logo-container">
-        <img
-          className="website-logo"
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-          alt="website logo"
-        />
+const Header = props => {
+  const {history} = props
 
-        <button type="button" className="nav-mobile-btn">
+  const onClickLogout = () => {
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
+
+  return (
+    <nav className="nav-header">
+      <div className="nav-content">
+        <div className="nav-bar-mobile-logo-container">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
-            alt="nav logout"
-            className="nav-bar-img"
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
           />
-        </button>
+
+          <button
+            type="button"
+            className="nav-mobile-btn"
+            onClick={onClickLogout}
+          >
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+              alt="nav logout"
+              className="nav-bar-img"
+            />
+          </button>
+        </div>
+
+        <div className="nav-content nav-bar-large-container">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+          />
+          <ul className="nav-menu">
+            <Link className="nav-link" to="/">
+              <li className="nav-menu-item">Home</li>
+            </Link>
+
+            <Link className="nav-link" to="/products">
+              <li className="nav-menu-item">Products</li>
+            </Link>
+
+            <Link className="nav-link" to="/cart">
+              <li className="nav-menu-item">Cart</li>
+            </Link>
+          </ul>
+          <button
+            type="button"
+            className="logout-desktop-btn"
+            onClick={onClickLogout}
+          >
+            Logout
+          </button>
+        </div>
       </div>
+      <div className="nav-menu-mobile">
+        <ul className="nav-menu-list-mobile">
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+              alt="nav home"
+              className="nav-bar-img"
+            />
+          </li>
 
-      <div className="nav-content nav-bar-large-container">
-        <img
-          className="website-logo"
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
-          alt="website logo"
-        />
-        <ul className="nav-menu">
-          <li className="nav-menu-item">Home</li>
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+              alt="nav products"
+              className="nav-bar-img"
+            />
+          </li>
 
-          <li className="nav-menu-item">Products</li>
-
-          <li className="nav-menu-item">Cart</li>
+          <li className="nav-menu-item-mobile">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+              alt="nav cart"
+              className="nav-bar-img"
+            />
+          </li>
         </ul>
-        <button type="button" className="logout-desktop-btn">
-          Logout
-        </button>
       </div>
-    </div>
-    <div className="nav-menu-mobile">
-      <ul className="nav-menu-list-mobile">
-        <li className="nav-menu-item-mobile">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
-            alt="nav home"
-            className="nav-bar-img"
-          />
-        </li>
-
-        <li className="nav-menu-item-mobile">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
-            alt="nav products"
-            className="nav-bar-img"
-          />
-        </li>
-
-        <li className="nav-menu-item-mobile">
-          <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
-            alt="nav cart"
-            className="nav-bar-img"
-          />
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
-export default Header
+    </nav>
+  )
+}
+export default withRouter(Header)
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,3 +1,5 @@
+import Cookies from 'js-cookie'
+
 import {Component} from 'react'
 
 import './index.css'
@@ -18,9 +20,9 @@ class LoginForm extends Component {
     this.setState({password: event.target.value})
   }
 
-  onSubmitSuccess = () => {
+  onSubmitSuccess = jwtToken => {
     const {history} = this.props
-
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
     history.replace('/')
   }
 
@@ -40,7 +42,7 @@ class LoginForm extends Component {
     const response = await fetch(url, options)
     const data = await response.json()
     if (response.ok === true) {
-      this.onSubmitSuccess()
+      this.onSubmitSuccess(data.jwt_token)
     } else {
       this.onSubmitFailure(data.error_msg)
     }
