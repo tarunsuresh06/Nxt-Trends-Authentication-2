fb89860aabebd74078c77fff627f3b8903019999
tommy <tarunsuresh.v@gmail.com>
2023-05-18 12:18:05 +0000

    Fix Authenticate for Cart & Product Components

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,17 +1,26 @@
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import './index.css'
 import Header from '../Header'
 
-const Cart = () => (
-  <>
-    <Header />
-    <div className="cart-container">
-      <img
-        className="cart-image"
-        alt="cart"
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
-      />
-    </div>
-  </>
-)
+const Cart = () => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="cart-container">
+        <img
+          className="cart-image"
+          alt="cart"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        />
+      </div>
+    </>
+  )
+}
 
 export default Cart
--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,17 +1,26 @@
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import './index.css'
 import Header from '../Header'
 
-const Products = () => (
-  <>
-    <Header />
-    <div className="products-container">
-      <img
-        className="products-image"
-        alt="products"
-        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
-      />
-    </div>
-  </>
-)
+const Products = () => {
+  const jwtToken = Cookies.get('jwt_token')
+
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+  return (
+    <>
+      <Header />
+      <div className="products-container">
+        <img
+          className="products-image"
+          alt="products"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        />
+      </div>
+    </>
+  )
+}
 
 export default Products
