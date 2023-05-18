b059861a41a691b9c03e5dd87df3eba38ce6d3ff
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-18 16:25:24 +0530

    Add Cart & Products Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {Route, Switch} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
 import NotFound from './components/NotFound'
 
 import './App.css'
@@ -10,6 +12,8 @@ const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
     <Route exact path="/" component={Home} />
+    <Route exact path='/cart' component={Cart} />
+    <Route exact path='/products' component={Products} />
     <Route component={NotFound} />
   </Switch>
 )
