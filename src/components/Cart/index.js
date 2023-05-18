a8619abc22807ead383048c61b398cb64dbcc7ea
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-18 16:24:38 +0530

    Add Cart

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,1 +1,10 @@
-// Write your JS code here
+import './index.css'
+
+const Cart = () => (
+    <div className='cart-container'>
+        <img className='cart-image' alt='cart' src='https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png' />
+    </div>
+)
+
+
+export default Cart
