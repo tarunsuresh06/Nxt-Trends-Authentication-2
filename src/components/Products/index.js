93e395acb1f47469dc9f259ffcbbcc21288141d7
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-18 16:28:15 +0530

    Add Products

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,1 +1,10 @@
-// Write your JS code here
+import './index.css'
+
+const Products = () => (
+    <div className='products-container'>
+        <img className='products-image' alt='products' src='https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png' />
+    </div>
+)
+
+
+export default Products
